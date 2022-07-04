8701c4014b9eca6fcfeb22b11620b7e261fd7b70
YounByoungil <jimmy7525@naver.com>
2022-07-04 18:16:44 +0900

    delete project

---
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by 윤병일 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
